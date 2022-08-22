c70802315fb99481d8683f62abe04368df750c3e
Allan <allan.kowiti@student.moringaschool.com>
2022-08-22 15:47:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
- function addingEventListener() {
+function addingEventListener() {
     const input = document.getElementById('button');
     function clickAlert() { alert('I was clicked');}
     input.addEventListener('click', clickAlert);
 }
-
-
- 
 
 
 
